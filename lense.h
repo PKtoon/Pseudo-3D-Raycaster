ba4cdda13e346ee5e7f509e7b19735c638837f8f
PK2n <pratik_khanke005@yahoo.com>
2019-12-07 01:13:23 +0530

    better and maybe accurate lense algorithms, including fisheye effect lense

---
+++ lense.h
@@ -0,0 +1,50 @@
+#ifndef LENSE_H_INCLUDED
+#define LENSE_H_INCLUDED
+
+// std::pair<int,int> nonfish2(int xpos, int ypos, double playerAngle, double fov, double deltaAngle, double distance)
+// {
+//     //still confusion in angles, sort it out
+//     double arm, depth;
+// 
+//     double alpha = abs((fov/2)-deltaAngle); //wrong here
+//     double beta = playerAngle - ((fov/2)-deltaAngle);
+// 
+//     depth = std::tan(alpha) * distance;
+//     arm = depth / std::sin(alpha);
+// 
+//     int x = (std::sin(beta) * arm) + xpos;
+//     int y = (std::cos(beta) * arm) + ypos;
+// 
+//     return std::pair<int,int>(x,y);
+// }
+
+std::pair<int,int> nonfish(int xpos, int ypos, double playerAngle, double fov, double deltaAngle, double distance)
+{
+    //still confusion in angles, sort it out
+    double arm, depth;
+
+    double alpha = (fov/2)-(fov-deltaAngle);
+    double absalpha = abs(alpha);
+    depth = (std::tan(absalpha)/std::sin(absalpha))*distance;
+    
+    double beta = playerAngle + alpha;
+
+    int x = depth * std::sin(beta) + xpos;
+    int y = depth * std::cos(beta) + ypos;
+
+    return std::pair<int,int>(x,y);
+}
+
+std::pair<int,int> fish(int xpos, int ypos, double playerAngle, double fov, double deltaAngle, double distance)
+{
+//     double alpha = fov/2 - deltaAngle;
+    double alpha = (fov/2)-(fov-deltaAngle);
+    double beta = playerAngle + alpha;
+    
+    int x = distance * std::sin(beta) + xpos;
+    int y = distance * std::cos(beta) + ypos;
+    
+    return std::pair<int,int>(x,y);
+}
+
+#endif // LENSE_H_INCLUDED
