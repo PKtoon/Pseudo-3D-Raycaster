034b4ddac4201d389c90b44497e182d535730e84
PK2n <pratik_khanke005@yahoo.com>
2019-12-07 02:30:40 +0530

    More cleanup

--- lense.h
+++ lense.h
@@ -1,23 +1,6 @@
 #ifndef LENSE_H_INCLUDED
 #define LENSE_H_INCLUDED
 
-// std::pair<int,int> nonfish2(int xpos, int ypos, double playerAngle, double fov, double deltaAngle, double distance)
-// {
-//     //still confusion in angles, sort it out
-//     double arm, depth;
-// 
-//     double alpha = abs((fov/2)-deltaAngle); //wrong here
-//     double beta = playerAngle - ((fov/2)-deltaAngle);
-// 
-//     depth = std::tan(alpha) * distance;
-//     arm = depth / std::sin(alpha);
-// 
-//     int x = (std::sin(beta) * arm) + xpos;
-//     int y = (std::cos(beta) * arm) + ypos;
-// 
-//     return std::pair<int,int>(x,y);
-// }
-
 std::pair<int,int> nonfisheye(int xpos, int ypos, double playerAngle, double fov, double deltaAngle, double distance)
 {
     double alpha = (fov/2)-(fov-deltaAngle);
